adae74373663503003ad3b1c5f50c790cf51b5c7
dlwngus <dlwngus1129@naver.com>
2022-01-17 12:41:43 +0900

    idk what is it

---
+++ first.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int len;
+    int min = 0, max = 0;
+    char arr[] = {0};
+    
+    scanf("%d", &len);
+
+    for(int i = 0; i < len; i++)
+        scanf("%d", &arr[i]);
+
+
+    max = arr[0], min = arr[0];
+
+    for(int i = 0; i < len; i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("%d %d", min, max);
+}
